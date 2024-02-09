f5ba7e2f903a091f330d5820a5e7e436e0aca543
Glenn <casetoolbca201@gmail.com>
2024-02-10 00:56:26 +0530

    second

--- src/q1/Caterpillar_xxx.java
+++ src/q1/Caterpillar_xxx.java
@@ -1,5 +1,14 @@
 package q1;
 
 public class Caterpillar_xxx {
+	
+		int moves, cabbages;
+		void Caterpillar(int moves,int cabbages){
+			this.cabbages=cabbages;
+			this.moves=moves;
+	
+
+	}
+
 
 }
